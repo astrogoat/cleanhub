6726acf05e4f92ddebd90a0832b2ba34a7a29882
garos4 <benmba4@gmail.com>
2025-03-09 15:30:17 +0000

    Add client id to cleanhub settings

--- src/Settings/CleanhubSettings.php
+++ src/Settings/CleanhubSettings.php
@@ -7,12 +7,12 @@ use Illuminate\Validation\Rule;
 
 class CleanhubSettings extends AppSettings
 {
-    // public string $url;
+    public string $client_id;
 
     public function rules(): array
     {
         return [
-//            'url' => Rule::requiredIf($this->enabled === true), // Example, modify to fit your need.
+           'client_id' => Rule::requiredIf($this->enabled === true),
         ];
     }
 
