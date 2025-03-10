3fbcf1b9ad4e5ea4071f83d07c9c5d6409e46ce4
garos4 <benmba4@gmail.com>
2025-03-10 13:00:05 +0000

    update script blade file

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,12 +1,15 @@
-@if (Astrogoat\Cleanhub\Settings\CleanhubSettings::isEnabled() && !blank(settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'client_id')))
-    @once
-        <script type="text/javascript" src="{{ settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'url') }}">
+@if (Astrogoat\Cleanhub\Settings\CleanhubSettings::isEnabled() &&
+        !blank(settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'client_id')))
+    @push('strata:frontend:end')
+        @once
+            <script type="text/javascript" src="{{ settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'url') }}">
+            </script>
+        @endonce
+        <script type="text/javascript">
+            CleanhubBanner.mount({
+                clientId: "{{ settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'client_id') }}",
+                elementId: "cleanhub-banner"
+            })
         </script>
-    @endonce
-    <script type="text/javascript">
-        CleanhubBanner.mount({
-            clientId: "{{ settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'client_id') }}",
-            elementId: "cleanhub-banner"
-        })
-    </script>
+    @endpush
 @endif
