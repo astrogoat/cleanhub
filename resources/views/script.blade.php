07dd72accce9c04e3ee7d0709cd294c39e1adfb0
Benjamin Mba <44570779+garos4@users.noreply.github.com>
2025-06-09 14:57:03 +0000

    DTCE-2867: Cleanhub strata module disappearing (#2)

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,15 +1,12 @@
 @if (Astrogoat\Cleanhub\Settings\CleanhubSettings::isEnabled() &&
         !blank(settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'client_id')))
-    @push('strata:frontend:end')
-        @once
-            <script type="text/javascript" src="https://cdn.cleanhub.io/embed/CleanhubBanner.js">
-            </script>
-        @endonce
-        <script type="text/javascript">
-            CleanhubBanner.mount({
-                clientId: "{{ settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'client_id') }}",
-                elementId: "cleanhub-banner"
-            })
-        </script>
-    @endpush
+    @once
+        <script type="text/javascript" src="https://cdn.cleanhub.io/embed/CleanhubBanner.js"></script>
+    @endonce
+    <script type="text/javascript">
+        CleanhubBanner.mount({
+            clientId: "{{ settings(Astrogoat\Cleanhub\Settings\CleanhubSettings::class, 'client_id') }}",
+            elementId: "cleanhub-banner"
+        })
+    </script>
 @endif
